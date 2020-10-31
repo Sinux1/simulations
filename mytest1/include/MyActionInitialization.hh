33670d9bcbd28722ba4234abca627893b2a2885e
sinux <sinux1@programmer.net>
2020-10-30 18:52:07 -0700

    MyRunAction.hh created, refactored, from B2RunAction.hh My*Action.hh headers created

--- mytest1/include/MyActionInitialization.hh
+++ mytest1/include/MyActionInitialization.hh
@@ -1,4 +1,4 @@
-//
+ //
 // Created by Samad Mazarei on 10/19/20.
 // Copyright (c) 2020 California State University Channel Islands. All rights reserved.
 //
---
+++ mytest1/include/MyPrimaryGeneratorAction.hh
@@ -0,0 +1,9 @@
+//
+// Created by Samad Mazarei on 10/30/20.
+// Copyright (c) 2020 California State University Channel Islands. All rights reserved.
+//
+
+#ifndef MYTEST1_MYPRIMARYGENERATORACTION_HH
+#define MYTEST1_MYPRIMARYGENERATORACTION_HH
+
+#endif //MYTEST1_MYPRIMARYGENERATORACTION_HH
