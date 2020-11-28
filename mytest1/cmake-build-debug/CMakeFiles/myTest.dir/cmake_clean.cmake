08ab77643ec19a8af3abfa6ea97dc0037ebae52f
sinux <sinux1@programmer.net>
2020-11-28 03:54:41 -0800

    deleted some

--- mytest1/cmake-build-debug/CMakeFiles/myTest.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/myTest"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/myTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
